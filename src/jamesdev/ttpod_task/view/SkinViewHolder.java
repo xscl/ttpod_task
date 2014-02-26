be33f4e22cccc6ce5eb67bc7d69d6d657d38500e
Jiang Benpeng <jbp.130@163.com>
2014-02-26 11:04:37 +0800

    add comment in SkinViewHolder.java

--- src/jamesdev/ttpod_task/view/SkinViewHolder.java
+++ src/jamesdev/ttpod_task/view/SkinViewHolder.java
@@ -14,22 +14,32 @@ import jamesdev.ttpod_task.util.FileHelper;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * Created by Benpeng.Jiang
+ */
 public class SkinViewHolder {
+    private String mThumbName;
+    private Context mContext;
+    private BaseAdapter mBaseAdapter;
+    private static final String TAG = "SkinViewHolder";
+
     public ImageView imageView;
     public ProgressBar progressBar;
     public ImageView deleteImageView;
 
     public String skinUrl;
-    private String mThumbName;
+
     public boolean isEmbeded;
     public boolean isLast;
-
     public int position;
 
-    private Context mContext;
-    private BaseAdapter mBaseAdapter;
-    private static final String TAG = "SkinViewHolder";
 
+    /**
+     * Constructor
+     * @param context Activity whitch creates adapter
+     * @param baseAdapter Adapter whitch holds this object
+     * @param p position in adapter
+     */
     public SkinViewHolder(Context context, BaseAdapter baseAdapter, int p) {
         mContext = context;
         mBaseAdapter =baseAdapter;
@@ -37,16 +47,18 @@ public class SkinViewHolder {
         Log.d(TAG, "create new one, position:" + position);
     }
 
+    // called when clicked in view mode
     public void doResponse() {
         if (this.isEmbeded || FileHelper.isSkinExist(this.mThumbName)) {
             setSkin();
-        } else if (this.isLast ) {
+        } else if (this.isLast) {
             downloadMore();
         } else {
             startSkinDownload();
         }
     }
 
+    // called when clicked in edit mode
     public void doEdit() {
         Log.d(TAG, "edit skin");
         if (!this.isEmbeded && FileHelper.deleteSkin(this.mThumbName)) {
@@ -70,6 +82,11 @@ public class SkinViewHolder {
         Log.d(TAG, "download more skins");
     }
 
+    /**
+     * load preview image of skin
+     * @param thumbName skin name
+     * throws IOException
+     */
     public void loadThumb(String thumbName) {
         this.mThumbName = thumbName;
 
@@ -94,6 +111,10 @@ public class SkinViewHolder {
         }
     }
 
+    /**
+     * show or hide delete icon
+     * @param thumbName
+     */
     public void setDeleteImageView(String thumbName) {
         mThumbName = thumbName;
         if (FileHelper.isSkinExist(mThumbName)) {
@@ -103,6 +124,10 @@ public class SkinViewHolder {
         }
     }
 
+    /**
+     * return skin name
+     * @return skin name
+     */
     public String getThumbName() {
         return mThumbName;
     }
